1766bc48dc42fc32579da0761baf3221aacd5691
Shengyou Fan <shengyoufan@gmail.com>
2022-08-23 22:31:57 +0800

    connect to the database

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,10 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import org.jetbrains.exposed.sql.Database
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    Database.connect(
+        url = "jdbc:mysql://localhost:8889/sample",
+        driver = "com.mysql.cj.jdbc.Driver",
+        user = "root",
+        password = "root"
+    )
 }
