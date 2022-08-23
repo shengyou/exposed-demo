95f2835c4a4b39c907314e3e1284b717212a60f2
Shengyou Fan <shengyoufan@gmail.com>
2022-08-23 23:39:24 +0800

    create database script

---
+++ src/main/kotlin/io/kraftsman/exposed/Main.kt
@@ -0,0 +1,23 @@
+package io.kraftsman.exposed
+
+import io.kraftsman.exposed.tables.Authors
+import io.kraftsman.exposed.tables.Books
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
+
+fun main() {
+    Database.connect(
+        url = "jdbc:mysql://localhost:8889/sample",
+        driver = "com.mysql.cj.jdbc.Driver",
+        user = "root",
+        password = "root"
+    )
+
+    transaction {
+        SchemaUtils.create(
+            Books,
+            Authors,
+        )
+    }
+}
