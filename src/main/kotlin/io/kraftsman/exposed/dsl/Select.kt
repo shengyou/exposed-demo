0e50051dbecfd32184a0abbfbebadff30d73f253
Shengyou Fan <shengyoufan@gmail.com>
2022-08-23 23:39:49 +0800

    select data using dsl

---
+++ src/main/kotlin/io/kraftsman/exposed/dsl/Select.kt
@@ -0,0 +1,29 @@
+package io.kraftsman.exposed.dsl
+
+import io.kraftsman.exposed.tables.Books
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SortOrder
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
+
+fun main() {
+    Database.connect(
+        url = "jdbc:mysql://localhost:8889/sample",
+        driver = "com.mysql.cj.jdbc.Driver",
+        user = "root",
+        password = "root"
+    )
+
+    transaction {
+        Books.selectAll().forEach {
+            println("\"${it[Books.title]}\" published by ${it[Books.publisher]}")
+        }
+
+        Books.select { Books.id lessEq 5 }
+            .orderBy(Books.id to SortOrder.DESC)
+            .forEach {
+                println("(${it[Books.id]}) ${it[Books.title]}")
+            }
+    }
+}
