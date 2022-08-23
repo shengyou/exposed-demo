83f354e1efceaa331dbcaac9ce06933be07ea884
Shengyou Fan <shengyoufan@gmail.com>
2022-08-24 00:20:41 +0800

    delete data using dao

---
+++ src/main/kotlin/io/kraftsman/exposed/dao/Delete.kt
@@ -0,0 +1,19 @@
+package io.kraftsman.exposed.dao
+
+import io.kraftsman.exposed.entities.Author
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.transactions.transaction
+
+fun main() {
+    Database.connect(
+        url = "jdbc:mysql://localhost:8889/sample",
+        driver = "com.mysql.cj.jdbc.Driver",
+        user = "root",
+        password = "root"
+    )
+
+    transaction {
+        val author = Author.findById(1)
+        author?.delete()
+    }
+}
